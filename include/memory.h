347c5d364b8189bfad3e664c6d7c10c281f108ce
nyr1k <zolamanuraly@gmail.com>
2026-06-22 17:52:39 +0500

    header for ram related stuff

---
+++ include/memory.h
@@ -0,0 +1,27 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <stdint.h> 
+
+#define MEMORY_SIZE 1024 * 1024 * 4 /* 4 MB memory */
+
+#define BASE_ADDRESS 0x80000000
+#define MMIO_BASE 0x10000000
+#define END_ADDRESS (BASE_ADDRESS + MEMORY_SIZE)
+
+typedef struct {
+  uint8_t memory[MEMORY_SIZE]; 
+} ram_t;    
+
+typedef enum : uint8_t {
+  INVALID_ADDRESS,
+  MMIO_ADDRESS,
+  RAM_ADDRESS
+} memory_segment;
+
+void memory_init(); 
+
+uint32_t read_word(uint32_t address);
+void write_word(uint32_t address, uint32_t data);
+
+#endif 
