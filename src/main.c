bdfa057406346164f7da3a98ac4ef2db8496c647
nyr1k <zolamanuraly@gmail.com>
2026-06-22 23:18:49 +0500

    testing functions

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <cpu.h>
+#include <memory.h>
+#include <err.h>
 
 int main(int argc, char **argv) {
 
-  printf("%d\n", argc);  
-
+  printf("%d\n", cpu.regs[5]);  
+  memory_init();
+  
+  write_word(BASE_ADDRESS, -1);
+  printf("%zu\n", read_word(BASE_ADDRESS));
   return 0;
 }
