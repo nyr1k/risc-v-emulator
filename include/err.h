4fd01d2d658ff97ac3524887ea17ec8665fa5299
nyr1k <zolamanuraly@gmail.com>
2026-06-22 23:17:57 +0500

    add INVALID_WRITE_ADDRESS error code

--- include/err.h
+++ include/err.h
@@ -6,7 +6,8 @@
 
 typedef enum {
     INVALID_MEMORY_ACCESS,
-    INVALID_READ_ADDRESS 
+    INVALID_READ_ADDRESS,
+    INVALID_WRITE_ADDRESS
 } err_code;
 
 static const char* error_sentence = "[ERR] Aborting the program:";
@@ -20,6 +21,10 @@ static void report_and_abort(err_code error)
             break;
         case INVALID_READ_ADDRESS:
             printf("%s INVALID_READ_ADDRESS (%d)\n\n", error_sentence, INVALID_READ_ADDRESS);
+            abort();
+            break;
+        case INVALID_WRITE_ADDRESS:
+            printf("%s INVALID_WRITE_ADDRESS (%d)\n\n", error_sentence, INVALID_WRITE_ADDRESS);
             abort();
             break;
         default:
