e157ddb1a2efef6c0b9171025ee8251b0379d644
nyr1k <zolamanuraly@gmail.com>
2026-06-23 19:57:27 +0500

    add INVALID_INSTRUCTION error

--- include/err.h
+++ include/err.h
@@ -7,7 +7,8 @@
 typedef enum {
     INVALID_MEMORY_ACCESS,
     INVALID_READ_ADDRESS,
-    INVALID_WRITE_ADDRESS
+    INVALID_WRITE_ADDRESS,
+    INVALID_INSTRUCTION
 } err_code;
 
 static const char* error_sentence = "[ERR] Aborting the program:";
@@ -19,14 +20,22 @@ static void report_and_abort(err_code error)
             printf("%s INVALID_MEMORY_ACCESS (%d)\n\n", error_sentence, INVALID_MEMORY_ACCESS);
             abort();
             break;
+
         case INVALID_READ_ADDRESS:
             printf("%s INVALID_READ_ADDRESS (%d)\n\n", error_sentence, INVALID_READ_ADDRESS);
             abort();
             break;
+
         case INVALID_WRITE_ADDRESS:
             printf("%s INVALID_WRITE_ADDRESS (%d)\n\n", error_sentence, INVALID_WRITE_ADDRESS);
             abort();
             break;
+
+        case INVALID_INSTRUCTION:
+            printf("%s INVALID_INSTRUCTION (%d)\n\n", error_sentence, INVALID_INSTRUCTION);
+            abort();
+            break;
+
         default:
             break;
     }
