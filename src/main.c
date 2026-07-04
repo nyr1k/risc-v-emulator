8be744381e6b17b098ba25032d52812d2d71ef42
nyr1k <zolamanuraly@gmail.com>
2026-07-04 23:40:34 +0500

    pass programm arguments, create an instance of the emulator and initialize it

--- src/main.c
+++ src/main.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
+#include <emulator.h>
 #include <cpu.h>
 #include <memory.h>
 #include <err.h>
+#include <misc.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
 
-  printf("%d\n", cpu.regs[5]);  
-  memory_init();
-  
-  write_word(BASE_ADDRESS, -1);
-  printf("%zu\n", read_word(BASE_ADDRESS));
+  if (argc == 1) {
+    printf("Path to the ELF file is required!\n");
+    return 1;
+  } else if (argc >= 3) {
+    printf("too many arguments...\n");
+    return 1;
+  }
+
+  emulator_t rv32i;   
+  emulator_init(&rv32i, argv[1]);
+
+//   dump_cpu();
+
+//   printf("instr=0x%08X opcode=0x%02X rd=%u funct3=%u rs1=%u rs2=%u funct7=0x%02X\n",
+//     instr,
+//     decoded_instr.opcode,
+//     decoded_instr.rd,
+//     decoded_instr.funct3,
+//     decoded_instr.rs1,
+//     decoded_instr.rs2,
+//     decoded_instr.funct7
+// );
+
   return 0;
 }
