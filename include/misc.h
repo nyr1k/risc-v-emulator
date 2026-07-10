3ad826f9e8c90424fb243e8a18221e2ff7bbe1c1
nyr1k <zolamanuraly@gmail.com>
2026-07-10 18:44:30 +0500

    fix arithmetic shift right

--- include/misc.h
+++ include/misc.h
@@ -24,7 +24,7 @@ static inline int32_t sign_extend(uint32_t value, int bits)
 
 static inline uint32_t shift_right_arith(uint32_t value, int bits)
 {
-    return ~((~value) >> (bits));
+    return ~(((int32_t)(~value)) >> (bits));
 }
 
 #endif
